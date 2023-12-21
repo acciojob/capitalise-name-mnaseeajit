52d57a7e20b46eac680c6f7e9acb0062e4e40f5c
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-21 17:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+            var inputField = document.getElementById('fname');
+
+            // Add event listener to input field
+            inputField.addEventListener('blur', function () {
+                // Convert the content to uppercase
+                inputField.value = inputField.value.toUpperCase();
+            });
+      
